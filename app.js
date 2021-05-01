21589da34d99f279ba58784f059419404c924580
EliasTG <tg.elias.s@gmail.com>
2021-05-01 13:57:39 -0500

    Creadting file app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 200,
+        dist: -80,
+        shift: 5,
+        numVisible: 3,
+        indicators: true,
+    });
+});
+
