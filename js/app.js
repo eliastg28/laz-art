d36546eda11aedd2336971874b3296791a0b4b05
EliasTG <tg.elias.s@gmail.com>
2021-05-31 11:52:47 -0500

    Solving errors js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elementosCarousel = document.querySelectorAll('.carousel');
-    M.Carousel.init(elementosCarousel, {
-        duration: 200,
-        dist: -80,
-        shift: 5,
-        numVisible: 3,
-        indicators: true,
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  const elementosCarousel = document.querySelectorAll(".carousel");
+  M.Carousel.init(elementosCarousel, {
+    duration: 200,
+    dist: -80,
+    shift: 5,
+    numVisible: 3,
+    indicators: true,
+  });
 });
-
